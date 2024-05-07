402c88d8923411c23f4108b0d23f25c5194d275a
forestsoob <vini20022203@gmail.com>
2024-05-07 22:19:31 +0900

    영심's 4단계 감자

--- project/users/models.py
+++ project/users/models.py
@@ -1,20 +1,3 @@
-from django.contrib.auth.models import User
 from django.db import models
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    department = models.CharField(max_length=100)
-    hobby = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.user.username
-    
-class Post(models.Model):
-    title = models.CharField(max_length=50)
-    writer = models.CharField(max_length=30)
-    body = models.TextField()
-    pub_date = models.DateTimeField()
-
-
-    def __str__(self):
-        return self.title
+# Create your models here.
--- project/users/views.py
+++ project/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from main.models import Post
 
 # Create your views here.
 
